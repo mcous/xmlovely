6ffe57b870cade8e26c626f9628bb4884343a76e
Mike Cousins <mike@cousins.io>
2015-12-23 18:25:18 -0600

    Finish initial development

--- index.js
+++ index.js
@@ -1,15 +1,116 @@
 // xml pretty printer as a node transform stream
 'use strict'
 
+var StringDecoder = require('string_decoder').StringDecoder
 var inherits = require('inherits')
 var Transform = require('readable-stream').Transform
+var repeat = require('lodash.repeat')
+var reduce = require('lodash.reduce')
+var isFinite = require('lodash.isFinite')
 
-var PrettyPrinter = function() {
+var EOL = require('os').EOL
+var OPEN = '<'
+var CLOSE = '>'
+var END = '/'
+var ATTRIBUTE = '"'
+
+var PrettyPrinter = function(width, whitespace) {
   Transform.call(this)
+
+  this._decoder = new StringDecoder('utf8')
+  this._level = 0
+  this._width = width
+  this._tab = repeat(whitespace, width)
+  this._attribute = false
+  this._index = 0
+  this._lookingForNodeEnd = false
+  this._newLevel = false
+  this._closingNode = false
 }
 
 inherits(PrettyPrinter, Transform)
 
-module.exports = function() {
-  return new PrettyPrinter()
+PrettyPrinter.prototype._indent = function() {
+  return repeat(this._tab, this._level)
+}
+
+PrettyPrinter.prototype._transform = function(chunk, encoding, done) {
+  var stringChunk = this._decoder.write(chunk)
+
+  var newChunk = reduce(stringChunk, function(result, char, index) {
+    this._index += index
+
+    if (!this._attribute || (char === ATTRIBUTE)) {
+      if (this._newLevel) {
+        this._newLevel = false
+
+        if (!this._closingNode) {
+          this._level++
+        }
+        this._closingNode = false
+
+        if (char === OPEN) {
+          this._lookingForNodeEnd = true
+          return result
+        }
+
+        return result + this._indent() + char
+      }
+
+      if (this._lookingForNodeEnd) {
+        this._lookingForNodeEnd = false
+
+        if (char === END) {
+          this._closingNode = true
+          this._level--
+        }
+
+        return result + this._indent() + OPEN + char
+      }
+
+      if (char === OPEN) {
+        if (this._index) {
+          result += EOL
+        }
+
+        this._lookingForNodeEnd = true
+        return result
+      }
+
+      if (char === CLOSE) {
+        this._newLevel = true
+        return result + char + EOL
+      }
+
+      if (char === END) {
+        this._level--
+      }
+      else if (char === ATTRIBUTE) {
+        this._attribute = !this._attribute
+      }
+    }
+
+    return result + char
+  }, '', this)
+
+  this.push(newChunk)
+  done()
+}
+
+var parseOptions = function(opts) {
+  if (isFinite(opts)) {
+    return {width: opts, whitespace: ' '}
+  }
+
+  opts = opts || {}
+
+  return {
+    width: opts.width || 2,
+    whitespace: opts.whitespace || ' '
+  }
+}
+
+module.exports = function(opts) {
+  var options = parseOptions(opts)
+  return new PrettyPrinter(options.width, options.whitespace)
 }
--- test.js
+++ test.js
@@ -13,4 +13,187 @@ describe('xmlovely', function() {
   it('should be a transform stream', function() {
     expect(pretty).to.be.an.instanceOf(Transform)
   })
+
+  it('should place opening and closing tags on newlines', function(done) {
+    pretty.once('data', function(result) {
+      expect(result.toString()).to.equal('<tag>\n</tag>\n')
+      done()
+    })
+
+    pretty.write('<tag></tag>')
+  })
+
+  it('should indent children by two spaces', function(done) {
+    var expected = [
+      '<tag>',
+      '  <child/>',
+      '  <child/>',
+      '</tag>',
+      ''
+    ].join('\n')
+
+    pretty.once('data', function(result) {
+      expect(result.toString()).to.equal(expected)
+      done()
+    })
+
+    pretty.write('<tag><child/><child/></tag>')
+  })
+
+  it('should be able to handle text children', function(done) {
+    var expected = [
+      '<tag>',
+      '  some text',
+      '</tag>',
+      ''
+    ].join('\n')
+
+    pretty.once('data', function(result) {
+      expect(result.toString()).to.equal(expected)
+      done()
+    })
+
+    pretty.write('<tag>some text</tag>')
+  })
+
+  it('should be able to handle several nodes', function(done) {
+    var unpretty = [
+      '<foo>',
+      'some text',
+      '</foo>',
+      '<foo>',
+      '<bar>',
+      '<baz>',
+      '<qux/>',
+      '<foo/>',
+      '</baz>',
+      'hello',
+      '<baz/>',
+      '</bar>',
+      '</foo>'
+    ].join('')
+
+    var expected = [
+      '<foo>',
+      '  some text',
+      '</foo>',
+      '<foo>',
+      '  <bar>',
+      '    <baz>',
+      '      <qux/>',
+      '      <foo/>',
+      '    </baz>',
+      '    hello',
+      '    <baz/>',
+      '  </bar>',
+      '</foo>',
+      ''
+    ].join('\n')
+
+    pretty.once('data', function(result) {
+      expect(result.toString()).to.equal(expected)
+      done()
+    })
+
+    pretty.write(unpretty)
+  })
+
+  it('should ignore anything inside quotation marks', function(done) {
+    var unpretty = [
+      '<node>',
+      '<child attr="<ignore this/>"/>',
+      '<child attr="http://and-also-this.com"/>',
+      '</node>'
+    ].join('')
+    var expected = [
+      '<node>',
+      '  <child attr="<ignore this/>"/>',
+      '  <child attr="http://and-also-this.com"/>',
+      '</node>',
+      ''
+    ].join('\n')
+
+    pretty.once('data', function(result) {
+      expect(result.toString()).to.equal(expected)
+      done()
+    })
+
+    pretty.write(unpretty)
+  })
+
+  it('should take a single parameter that defaults to the tab width', function(done) {
+    pretty = xmlovely(6)
+
+    var unpretty = '<foo><bar><baz/></bar></foo>'
+    var expected = [
+      '<foo>',
+      '      <bar>',
+      '            <baz/>',
+      '      </bar>',
+      '</foo>',
+      ''
+    ].join('\n')
+
+    pretty.once('data', function(result) {
+      expect(result.toString()).to.equal(expected)
+      done()
+    })
+
+    pretty.write(unpretty)
+  })
+
+  it('should take an options object that sets the width and whitespace', function(done) {
+    pretty = xmlovely({width: 2, whitespace: '\t'})
+
+    var unpretty = '<foo><bar><baz/></bar></foo>'
+    var expected = [
+      '<foo>',
+      '\t\t<bar>',
+      '\t\t\t\t<baz/>',
+      '\t\t</bar>',
+      '</foo>',
+      ''
+    ].join('\n')
+
+    pretty.once('data', function(result) {
+      expect(result.toString()).to.equal(expected)
+      done()
+    })
+
+    pretty.write(unpretty)
+  })
+
+  it('should handle splits in weird places', function(done) {
+    var unpretty = [
+      '<node',
+      '><child',
+      ' attr="',
+      '</>"',
+      '/',
+      '>',
+      '<',
+      '/node>'
+    ]
+    var expected = [
+      '<node>',
+      '  <child attr="</>"/>',
+      '</node>',
+      ''
+    ].join('\n')
+
+    var result = ''
+    pretty.on('data', function(data) {
+      result += data
+    })
+
+    pretty.on('end', function() {
+      expect(result).to.equal(expected)
+      done()
+    })
+
+    unpretty.forEach(function(chunk) {
+      pretty.write(chunk)
+    })
+    pretty.end()
+  })
 })
